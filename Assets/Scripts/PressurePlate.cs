402c3ef0b05cb52a6ff3a9dde78d4a1e9c6d0266
Arnau-Camarero <72514159+Arnau-Camarero@users.noreply.github.com>
2024-06-01 17:11:06 +0200

    Change PressurePlate

--- Assets/Scripts/PressurePlate.cs
+++ Assets/Scripts/PressurePlate.cs
@@ -1,20 +1,60 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Netcode;
 using UnityEngine;
 
-public class PressurePlate : MonoBehaviour
+public class PressurePlate : NetworkBehaviour
 {
     public bool isPressed = false;
+    private NetworkVariable<bool> platePressed = new NetworkVariable<bool>(false);
 
-    void OnTriggerEnter(Collider col){
-        if(col.gameObject.tag == "Player" || col.gameObject.tag== "Prop"){
-            isPressed = true;
+    public override void OnNetworkSpawn()
+    {
+        if (!IsOwner)
+        {
+            enabled = false;
+        }
+        else
+        {
+            platePressed.OnValueChanged += OnPlatePressedChanged;
         }
     }
 
-    void OnTriggerExit(Collider col){
-        if(col.gameObject.tag == "Player" || col.gameObject.tag== "Prop"){
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Player" || col.gameObject.tag == "Prop")
+        {
+            isPressed = true;
+            PlatePressedServerRpc(true);
+        }
+    }
+
+    void OnTriggerExit(Collider col)
+    {
+        if (col.gameObject.tag == "Player" || col.gameObject.tag == "Prop")
+        {
             isPressed = false;
+            PlatePressedServerRpc(false);
+        }
+    }
+
+    [ServerRpc]
+    void PlatePressedServerRpc(bool pressed)
+    {
+        platePressed.Value = pressed;
+    }
+
+    void OnPlatePressedChanged(bool oldValue, bool newValue)
+    {
+        isPressed = newValue;
+        Debug.Log($"Plate pressed state changed to: {newValue}");
+    }
+
+    private void OnDestroy()
+    {
+        if (IsOwner)
+        {
+            platePressed.OnValueChanged -= OnPlatePressedChanged;
         }
     }
 }
